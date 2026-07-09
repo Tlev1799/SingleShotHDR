b1f5f626f0235548f6cc57cf1df4c392419cdb22
Tal Levitan <tal.levitan@campus.technion.ac.il>
2026-07-09 15:15:06 +0300

    Updated hdr_dataset.py

--- datasets/hdr_dataset.py
+++ datasets/hdr_dataset.py
@@ -1,11 +1,10 @@
 from pathlib import Path
-
+import random
 import torch
-
-import os
-import utils.image_ops as img_ops
-
 from torch.utils.data import Dataset
+import torchvision.transforms.functional as TF
+
+import utils.image_ops as img_ops
 
 class HDRDataset(Dataset):
 
@@ -25,11 +24,9 @@ class HDRDataset(Dataset):
         img = img.permute(2, 0, 1)
 
         return img
-
+    
 class HDRTestDataset(Dataset):
-
     def __init__(self, root):
-        # Scans for both standard .tif and .tiff extensions
         root_path = Path(root)
         self.files = sorted(list(root_path.rglob("*.exr")))
 
@@ -38,14 +35,7 @@ class HDRTestDataset(Dataset):
 
     def __getitem__(self, idx):
         file_path = str(self.files[idx])
-        
-        # Load, resize, and convert to RGB
         img = img_ops.load_exr_image(file_path)
-
-        # Convert the float32/float16 numpy array to a PyTorch tensor
         img = torch.from_numpy(img).float()
-
-        # Permute from HWC (Height, Width, Channels) to CHW for PyTorch
         img = img.permute(2, 0, 1)
-
         return img
